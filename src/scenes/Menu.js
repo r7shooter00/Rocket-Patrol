65932bf12f54033c555a8c847a48bd4b167a27eb
Alain Kassarjian <alain.kassarjian@gmail.com>
2020-04-13 22:24:03 -0700

    Initial commit

---
+++ src/scenes/Menu.js
@@ -0,0 +1,16 @@
+class Menu extends Phaser.Scene
+{
+	constructor()
+	{
+		super("menuScene");
+	}
+
+	create()
+	{
+		//display menu text
+		this.add.text(20, 20, "Rocket Patrol Menu");
+
+		//launch the next scene
+		this.scene.start("playScene");
+	}
+}
